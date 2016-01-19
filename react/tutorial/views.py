58514134e68b081ecbcb1b94aedf5a86cf90cc70
Alex Clark <aclark@aclark.net>
2016-01-18 21:47:35 -0500

    Update

--- react/tutorial/views.py
+++ react/tutorial/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CommentSerializer
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows comments to be created.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
