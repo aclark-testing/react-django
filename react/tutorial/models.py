cdf164579907adc0dcc3413a3058ba894dd6ce82
Alex Clark <aclark@aclark.net>
2016-01-18 21:40:46 -0500

    Update

--- react/tutorial/models.py
+++ react/tutorial/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=300, blank=True, null=True)
+    name = models.CharField(max_length=300, blank=True, null=True)
